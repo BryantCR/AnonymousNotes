0f8e44c561623d1db99194b6a59b6931350a7d20
BryantCR <bryancvargas20@gmail.com>
2021-12-07 15:51:15 -0600

    ok

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { NotesComponent } from './notes/notes.component';
+import { NotesService } from './notes/notes.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [NotesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
