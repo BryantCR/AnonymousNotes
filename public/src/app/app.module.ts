6f84d254bf516ff8ccf873131a89e9ba7934d9e9
BryantCR <bryancvargas20@gmail.com>
2021-12-07 16:19:59 -0600

    Ok

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -16,6 +16,7 @@ import { NotesService } from './notes/notes.service';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [NotesService],
   bootstrap: [AppComponent]
